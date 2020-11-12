7207c8f7849d875d88266532217e546acdaa1034
Roman1236 <ivik2606@gmail.com>
2020-11-12 21:24:01 +0200

    Initial commit

---
+++ System-Modeling-8-master/src/Arc.java
@@ -0,0 +1,25 @@
+public class Arc {
+    public Transition nextTransition ;
+    public Place placeFrom;
+    public Place placeTo ;
+    public int n ; // кратність дуги
+    public String name ;
+
+    public Arc(String name, Place next, int n)
+    {
+        this.name = name;
+        placeTo = next;
+        this.n = n;
+    }
+
+    public Arc(String name, Place previousPlace, Transition nextTransition, int n)
+    {
+        this.name = name;
+        this.nextTransition = nextTransition;
+        placeFrom = previousPlace;
+        this.n = n;
+    }
+
+
+}
+
